1eb35997e4091c8ea5a113bfd1e7ac16096a59e8
Mikrzak <mikolajjuszczak17@gmail.com>
2025-05-13 19:53:50 +0200

    Added a card for instructions on the main page

--- flutterfrontend/lib/pages/MainPage.dart
+++ flutterfrontend/lib/pages/MainPage.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import '../api/ApiCaller.dart';
 
 class MainPage extends StatelessWidget {
-  const MainPage({super.key});
+  double screenWidth = 0;
+  double screenHeight = 0;
+
+  @override
+  MainPage({super.key});
 
   void showCustomModalBottomSheet(BuildContext context, String text) {
     showModalBottomSheet<void>(
@@ -10,7 +14,7 @@ class MainPage extends StatelessWidget {
       isScrollControlled: true,
       builder: (BuildContext context) {
         return Container(
-          height: 400,
+          height: screenWidth * 0.75,
           color: Colors.transparent,
           child: Center(
             child: Column(
@@ -19,13 +23,12 @@ class MainPage extends StatelessWidget {
 
               children: <Widget>[
                 Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  child: Text(
-                    textAlign: TextAlign.center,
-                    text,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: screenHeight * 0.05,
                   ),
+                  child: Text(textAlign: TextAlign.center, text),
                 ),
-                const SizedBox(height: 100),
+                SizedBox(height: screenHeight * 0.1),
               ],
             ),
           ),
@@ -34,8 +37,52 @@ class MainPage extends StatelessWidget {
     );
   }
 
+  Column customButton(BuildContext context, String text, IconData iconData) {
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: const Color(0XFFBAC7BD),
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: IconButton(
+            icon: Icon(
+              iconData,
+              size: screenHeight * 0.18,
+              color: Color.fromARGB(255, 192, 207, 200),
+            ),
+            onPressed: () async {
+              //String result = await ApiCaller.getResult();
+
+              // ScaffoldMessenger.of(context).showSnackBar(
+              //   SnackBar(content: Text(result)),
+              // );
+            },
+
+            style: IconButton.styleFrom(
+              backgroundColor: Colors.white54,
+              foregroundColor: Colors.white54,
+
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+
+              fixedSize: Size(screenWidth * 0.4, screenHeight * 0.25),
+            ),
+          ),
+        ),
+
+        Text(text, style: TextStyle(fontWeight: FontWeight.normal)),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    screenWidth = MediaQuery.of(context).size.width;
+    screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
@@ -46,14 +93,20 @@ class MainPage extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.question_mark),
             onPressed: () {
-              showCustomModalBottomSheet(context, 'Tutaj pojawią się informacje na temat działania aplikacji, porady korzystania z niej, informacje dotyczące zdjęć itd.');
+              showCustomModalBottomSheet(
+                context,
+                'Tutaj pojawią się informacje na temat działania aplikacji, porady korzystania z niej, informacje dotyczące zdjęć itd.',
+              );
             },
           ),
 
           IconButton(
             icon: const Icon(Icons.info),
             onPressed: () {
-              showCustomModalBottomSheet(context, 'Tutaj pojawią się informacje na temat autorów i projektu.');
+              showCustomModalBottomSheet(
+                context,
+                'Tutaj pojawią się informacje na temat autorów i projektu.',
+              );
             },
           ),
         ],
@@ -62,122 +115,52 @@ class MainPage extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.start,
+
         children: <Widget>[
-          SizedBox(height: 20),
+          SizedBox(height: screenHeight * 0.07),
 
           Center(
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Column(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        //border: Border.all(color: Colors.black, width: 2),
-                        color: const Color(0XFFBAC7BD),
-                        shape: BoxShape.rectangle,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: IconButton(
-                        icon: const Icon(
-                          Icons.image_rounded,
-                          size: 100,
-                          color: Color.fromARGB(255, 192, 207, 200),
-                        ),
-                        iconSize: 100,
-                        //padding: EdgeInsets.all(0),
-                        onPressed: () async {
-                          //String result = await ApiCaller.getResult();
+                customButton(context, 'Wybierz zdjęcie', Icons.image_rounded),
 
-                          // ScaffoldMessenger.of(context).showSnackBar(
-                          //   SnackBar(content: Text(result)),
-                          // );
-                        },
+                SizedBox(width: screenHeight * 0.05),
 
-                        //child: const Text('Take a photo'),
-                        style: IconButton.styleFrom(
-                          backgroundColor:
-                              Colors
-                                  .white54, //const Color.fromARGB(255, 255, 250, 206),
-                          foregroundColor: Colors.white54,
-
-                          //textStyle: const TextStyle(fontSize: 20),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                              8,
-                            ), // Rounded corners
-                          ),
-
-                          fixedSize: Size(200, 250),
-                        ),
-                      ),
-                    ),
-
-                    Text(
-                      'Zrób zdjęcie',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-
-                SizedBox(width: 20),
-
-                Column(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        //border: Border.all(color: Colors.black, width: 2),
-                        color: const Color(0XFFBAC7BD),
-                        shape: BoxShape.rectangle,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: IconButton(
-                        icon: const Icon(
-                          Icons.camera_alt_rounded,
-                          size: 100,
-                          color: Color.fromARGB(255, 192, 207, 200),
-                        ),
-                        iconSize: 100,
-                        //padding: EdgeInsets.all(0),
-                        onPressed: () async {
-                          //String result = await ApiCaller.getResult();
-
-                          // ScaffoldMessenger.of(context).showSnackBar(
-                          //   SnackBar(content: Text(result)),
-                          // );
-                        },
-
-                        //child: const Text('Take a photo'),
-                        style: IconButton.styleFrom(
-                          backgroundColor:
-                              Colors
-                                  .white54, //const Color.fromARGB(255, 255, 250, 206),
-                          foregroundColor: Colors.white54,
-
-                          //textStyle: const TextStyle(fontSize: 20),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                              8,
-                            ), // Rounded corners
-                          ),
-
-                          fixedSize: Size(200, 250),
-                        ),
-                      ),
-                    ),
-
-                    Text(
-                      'Prześlij zdjęcie',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
+                customButton(
+                  context,
+                  'Prześlij zdjęcie',
+                  Icons.camera_alt_rounded,
                 ),
               ],
             ),
           ),
+
+          SizedBox(height: screenHeight * 0.1),
+
+          Card(
+            elevation: 4,
+            color: const Color.fromARGB(255, 237, 235, 235),
+            //margin: EdgeInsets.all(16),
+            child: SizedBox(
+              width: screenWidth * 0.7,
+              height: screenHeight * 0.4,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: screenWidth * 0.05,
+                  vertical: screenHeight * 0.05,
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text('Kliknij ikonę znaku zapytania, aby uzyskać więcej informacji o aplikacji.\n\nKliknij ikonę info, aby uzyskać więcej informacji o autorach i projekcie.', textAlign: TextAlign.center),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
-      backgroundColor: const Color(0xFFF7F7F7),
     );
   }
 }
