dff568908f31b1dbaf1ff37a470d34d46befa30e
Mikrzak <mikolajjuszczak17@gmail.com>
2025-05-23 13:06:45 +0200

    Experimenting with the design, trying to make the colors work

--- flutterfrontend/lib/pages/MainPage.dart
+++ flutterfrontend/lib/pages/MainPage.dart
@@ -57,21 +57,25 @@ Material customButton(
 }) {
   return Material(
     elevation: 20,
-    borderRadius: BorderRadius.circular(8),
+    borderRadius: BorderRadius.circular(10),
 
     child: Column(
       children: [
         Container(
           decoration: BoxDecoration(
             shape: BoxShape.rectangle,
-            borderRadius: BorderRadius.circular(8),
-            gradient: const LinearGradient(
+            borderRadius: BorderRadius.circular(10),
+            gradient: const SweepGradient(
               colors: [
-                Color.fromARGB(255, 0, 0, 0),
-                Color.fromARGB(255, 61, 61, 61),
+                 Color.fromARGB(255, 209, 178, 146),
+                Color.fromARGB(255, 220, 171, 175),
+                Color.fromARGB(255,193,173,204),
+                  //Color.fromARGB(255, 155, 176, 208),
+                  Color.fromARGB(255, 209, 178, 146),
               ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
+              //radius: 0.1,
+              //begin: Alignment.topRight,
+              //end: Alignment.bottomLeft,
             ),
           ),
           child: IconButton(
@@ -84,7 +88,7 @@ Material customButton(
 
             style: IconButton.styleFrom(
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
+                borderRadius: BorderRadius.circular(10),
               ),
 
               fixedSize: Size(screenWidth * 0.4, screenHeight * 0.25),
@@ -95,44 +99,6 @@ Material customButton(
         //Text(text, style: TextStyle(fontWeight: FontWeight.normal)),
       ],
     ),
-  );
-}
-
-void showCustomModalBottomSheet(BuildContext context, String text) {
-  showModalBottomSheet<void>(
-    context: context,
-    isScrollControlled: true,
-    builder: (BuildContext context) {
-      return Container(
-        height: screenWidth * 0.75,
-        color: Colors.transparent,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: screenHeight * 0.05),
-                child: Text(
-                  textAlign: TextAlign.center,
-                  text,
-                  style: GoogleFonts.getFont(
-                    'DM Serif Text',
-                    textStyle: TextStyle(
-                      fontSize: screenHeight * 0.025,
-                      color: const Color.fromARGB(255, 0, 0, 0),
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: screenHeight * 0.1),
-            ],
-          ),
-        ),
-      );
-    },
   );
 }
 
@@ -155,6 +121,24 @@ class MainPageContent extends StatelessWidget {
         backgroundColor:
             Colors.white, //const Color.fromARGB(255, 237, 235, 235),
 
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              colors: [
+                Color.fromARGB(255, 220, 194, 168),
+                //Color.fromARGB(255,222,177,181),
+                //Color.fromARGB(255,193,173,204),
+                  Color.fromARGB(255, 155, 176, 208),
+              ],
+              //stops: [0.2, 0.3, 0.4, 0.6],
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+            ),
+          ),
+        ),
+
+        //shape: Border(bottom: BorderSide(color: const Color.fromARGB(255, 255, 205, 205), width: 1)),
+
         title: Text(
           'Diagnozer paznokci',
           style: GoogleFonts.getFont(
@@ -172,9 +156,7 @@ class MainPageContent extends StatelessWidget {
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(
-                  builder: (context) => const InfoPage(),
-                ),
+                MaterialPageRoute(builder: (context) => const InfoPage()),
               );
             },
           ),
@@ -185,22 +167,27 @@ class MainPageContent extends StatelessWidget {
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(
-                  builder: (context) => const AuthorsPage(),
-                ),
+                MaterialPageRoute(builder: (context) => const AuthorsPage()),
               );
             },
           ),
         ],
+
+        toolbarHeight: screenHeight * 0.09,
       ),
 
       body: Container(
         decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("../resources/waves.png"),
+            fit: BoxFit.cover,
+          ),
           gradient: const LinearGradient(
             colors: [
               Color.fromARGB(255, 255, 255, 255),
               Color.fromARGB(255, 225, 224, 224),
             ],
+
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
           ),
@@ -427,10 +414,7 @@ class AuthorsPage extends StatelessWidget {
                     text: 'Autorzy projektu:',
                     style: TextStyle(fontWeight: FontWeight.bold),
                   ),
-                  TextSpan(
-                    text:
-                        ' tutaj pojawią się autorzy projektu.',
-                  ),
+                  TextSpan(text: ' tutaj pojawią się autorzy projektu.'),
                 ],
               ),
               textAlign: TextAlign.center,
